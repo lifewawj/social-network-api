8ab85f37fefb82f731692b7e09b731fbdc55d237
lifewawj <arevaloaj01@gmail.com>
2024-03-05 04:05:35 -0800

    Fixed Routing Bug with addFriend and deleteFriend routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,7 +17,7 @@ router.route("/").get(getAllUsers).post(createUser);
 router.route("/:userId").get(getUserById).put(updateUserById).delete(deleteUserById);
 
 // /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/friendId").put(addFriend).delete(deleteFriend);
+router.route("/:userId/friends/:friendId").put(addFriend).delete(deleteFriend);
 
 
 module.exports = router;
